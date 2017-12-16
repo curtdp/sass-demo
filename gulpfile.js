fc8492af420802fccf10411e400bcaffed5dcc7d
Igor Oleynik <igor.oleynikdp@gmail.com>
2017-12-16 21:46:09 +0200

    Обновил проект до версии с Browsersync и в состоянии с которым мы закончили занятие

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
-'use strict';
+'use strict'
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function () {
+  browserSync.init({
+    server: './app',
+  })
+  gulp.watch('app/sass/*.scss', ['sass',])
+  gulp.watch('app/*.html').on('change', browserSync.reload)
+})
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+  return gulp
+    .src('app/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream())
+})
